a007dbfaaae50e78854bb1cd1c7a73121559178e
chiara.dentini1 <chiara.dentini@accenture.com>
2022-07-14 14:48:11 +0200

    merge

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
+//generate random color
 function randomColor() {
-  var base = "#";
-  var num = "123456789ABCDEF";
+  var base = "#"; //hexadecimal star always with #
+  var num = "123456789ABCDEF"; //hexadecimal have number from 1 to 9 and letters from a to f
+  //looping with max 6 (hexadecimal are composed with 6 numbers and letters mix)
   for (let i = 0; i < 6; i++) {
-    var indice = Math.floor(Math.random() * num.length);
-    var random = num[indice];
-    base += random;
+    //pick random numember and letters from my var num
+    var index = Math.floor(Math.random() * num.length);
+    var random = num[index]; //give to my num variable a random index
+    base += random; //using addition assignmente operator for add a value to my variable base
   }
   return base;
 }
