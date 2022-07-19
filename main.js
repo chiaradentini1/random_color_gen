55f3f95c832712bd6d180a4d6af13044eeed57bb
chiara.dentini1 <chiara.dentini@accenture.com>
2022-07-19 11:05:37 +0200

    add dropdown

--- main.js
+++ main.js
@@ -1,38 +1,37 @@
 var myArray = [];
-//generate random color
+//genero una funzione che mi ritorna numeri e lettere mischiati randomicamente
 function randomColor() {
-  var base = "#"; //hexadecimal star always with #
-  var num = "123456789ABCDEF"; //hexadecimal have number from 1 to 9 and letters from a to f
-  //loop with max 6 (hexadecimal are composed with 6 numbers and letters mix)
+  var base = "#";
+  var num = "123456789ABCDEF"; //gli esadecimali sono composti da numeri (1-9) e lettere (a-f)
   for (let i = 0; i < 6; i++) {
-    //pick random numember and letters from my var num
+    //prendo randomicamente numeri e lettere dalla variabile
     var index = Math.floor(Math.random() * num.length);
-    var random = num[index]; //give to my num variable a random index
-    base += random; //using addition assignmente operator for add a value to my variable base
+    var random = num[index];
+    base += random;
   }
   return base;
 }
 
 function print() {
-  var mainDiv = document.querySelector("#divPalette"); //get div in my html
+  var mainDiv = document.querySelector("#div-palette");
   for (let i = 0; i < 40; i++) {
-    var color = randomColor(); //set a for contain the random color
-    var myDiv = document.createElement("div"); //create a div
-    myDiv.style.backgroundColor = color; //the background color would be the result of my random color function
-    myDiv.setAttribute("data-color", `${color}`); //setting the attribute data- and the value is var color (my random color)
-    mainDiv.append(myDiv); //insert in my html
+    var color = randomColor();
+    var myDiv = document.createElement("div");
+    myDiv.style.backgroundColor = color;
+    myDiv.setAttribute("data-color", `${color}`);
+    mainDiv.append(myDiv);
     // console.log(color);
   }
 }
 print();
 
-var data = document.querySelectorAll("[data-color]"); //take the data- value
-var select = document.querySelector("#selection"); // take select from my html
+var data = document.querySelectorAll("[data-color]"); //prendo il valore del data-color
+var select = document.querySelector("#selection");
 for (let i = 0; i < data.length; i++) {
   const element = data[i];
-  myArray.push(element.dataset.color); //create an array with the value of my data-
+  myArray.push(element.dataset.color); //creo un array popolato dai valori del data-color
 }
-//create option element and add it to my select
+
 for (let i = 0; i < myArray.length; i++) {
   const element = myArray[i];
   var option = document.createElement("option");
@@ -43,14 +42,22 @@ for (let i = 0; i < myArray.length; i++) {
 select.addEventListener("change", (e) => {
   // console.log(e.currentTarget.value);
   for (let i = 0; i < data.length; i++) {
-    const element = data[i]; //take all my elements
+    const element = data[i];
     element.classList.remove("hidden");
     if (e.currentTarget.value == "") {
-      //if the value of the option click is empty string
-      element.classList.remove("hidden"); //remove class hidden (display: none)
+      element.classList.remove("hidden");
     } else if (element.dataset.color !== e.currentTarget.value) {
-      //if the value of my data-color is not equal to the value of option
-      element.classList.add("hidden"); //ad class hidden to all element who don't have the value
+      element.classList.add("hidden");
     }
   }
 });
+
+//hamburger menù
+function dropDown() {
+  let dropdownBtn = document.querySelector(".mobile-menu-btn");
+  let dropdownUl = document.querySelector(".menu-container");
+  dropdownBtn.addEventListener("click", function () {
+    dropdownUl.classList.toggle("hidden");
+  });
+}
+dropDown();
